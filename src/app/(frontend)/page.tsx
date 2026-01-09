a428dda6216d4f7f212ac1198c7748214eb0adf8
EyosiSol <eyosila123@gmail.com>
2026-01-09 18:19:59 +0300

    feat: add tailwind for the frontend

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,3 @@
-export default async function HomePage() {
-  return <>Hola</>
+export default function Home() {
+  return <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
 }
