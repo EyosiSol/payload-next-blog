0a330fd2f85f32b4d167fac2893a7b145db1fc12
EyosiSol <eyosila123@gmail.com>
2026-01-09 17:50:12 +0300

    feat: implement article seeder and enhance content summary generation

--- src/collections/Articles/hooks/generate-contentSummary-hook.ts
+++ src/collections/Articles/hooks/generate-contentSummary-hook.ts
@@ -1,12 +1,9 @@
 import type { FieldHook } from 'payload'
-import { slugify } from 'payload/shared'
 import type { Article as ArticleDoc } from '@/payload-types'
 import { convertLexicalToPlaintext } from '@payloadcms/richtext-lexical/plaintext'
-
-const MAX_SUMMARY_LENGTH = 160
+import { MAX_SUMMARY_LENGTH } from '../constants'
 
 const generateContentSummaryHook: FieldHook<ArticleDoc, string> = ({ value, data }) => {
-  console.log(data?.content)
   if (value) return value.trim()
   if (!data?.content) return ''
   const text = convertLexicalToPlaintext({ data: data?.content }).trim()
